2b0b693487383a09e5bcb5ec1531c727eb3c0436
hemantt9l <hemant@t9l.com>
2020-09-06 19:32:41 +0530

    chnage project name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-v9'),
+      dir: require('path').join(__dirname, './coverage/eton-bikes'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
