5dd37268d533740183ca3ae0c97836b19aa11c35
manishvashisht15 <71211508+manishvashisht15@users.noreply.github.com>
2020-09-25 13:06:26 +0530

    Add files via upload

--- src/app/layouts/components/header/header.component.ts
+++ src/app/layouts/components/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core";
-import { interval } from "rxjs";
-import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-header",
@@ -8,42 +6,16 @@ import { map } from "rxjs/operators";
   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-  _diff: number;
-  _days: number;
-  _hours: number;
-  _minutes: number;
-  _seconds: number;
+
+  public bike_menu:boolean = false;
+  
   constructor() {}
 
   ngOnInit() {
-    interval(1000)
-      .pipe(
-        map((x) => {
-          this._diff =
-            Date.parse("2020-10-15") - Date.parse(new Date().toString());
-        })
-      )
-      .subscribe((x) => {
-        this._days = this.getDays(this._diff);
-        this._hours = this.getHours(this._diff);
-        this._minutes = this.getMinutes(this._diff);
-        this._seconds = this.getSeconds(this._diff);
-      });
+  }
+
+  showBikes(){
+    this.bike_menu = !this.bike_menu;
   }
   
-  getDays(t) {
-    return Math.floor(t / (1000 * 60 * 60 * 24));
-  }
-
-  getHours(t) {
-    return Math.floor((t / (1000 * 60 * 60)) % 24);
-  }
-
-  getMinutes(t) {
-    return Math.floor((t / 1000 / 60) % 60);
-  }
-
-  getSeconds(t) {
-    return Math.floor((t / 1000) % 60);
-  }
 }
