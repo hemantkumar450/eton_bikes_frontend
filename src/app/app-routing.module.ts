8770b6c317bff8619079ccb1b265c4ba22d9e6c6
manishvashisht15 <71211508+manishvashisht15@users.noreply.github.com>
2020-09-25 13:03:27 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { BikeComponent } from './modules/bike/bike.component';
+import { BikeDetailComponent } from './modules/bike/detail/bike-detail.component';
 
 const routes: Routes = [
   {
@@ -17,6 +19,8 @@ const routes: Routes = [
     loadChildren: () =>
       import("./auth/register/register.module").then((m) => m.RegisterModule),
   },
+  {path:'bike', component:BikeComponent},
+  {path:'bike-detail', component:BikeDetailComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,5 +9,8 @@ import { AppComponent } from "./app.component";
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class AppModule {}
