a6c8587bb6ff89749246a78e99e7405b6d5251de
hemantt9l <hemant@t9l.com>
2020-10-01 17:24:52 +0530

    changes

--- src/app/modules/bike/bike.component.ts
+++ src/app/modules/bike/bike.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnDestroy } from "@angular/core";
+import {
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  ViewChild,
+} from "@angular/core";
+import { NguCarousel, NguCarouselConfig } from "@ngu/carousel";
 
 @Component({
   selector: "app-bike",
@@ -6,5 +12,35 @@ import { Component, OnDestroy } from "@angular/core";
   styleUrls: ["./bike.component.scss"],
 })
 export class BikeComponent {
-  constructor() {}
+  name = "Angular";
+  slideNo = 0;
+  withAnim = true;
+  resetAnim = true;
+
+  @ViewChild("myCarousel") myCarousel: NguCarousel<any>;
+  carouselConfig: NguCarouselConfig = {
+    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+    load: 3,
+    interval: { timing: 4000, initialDelay: 1000 },
+    loop: true,
+    touch: true,
+    velocity: 0.2,
+  };
+  carouselItems = [1, 2, 3];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
+
+  reset() {
+    this.myCarousel.reset(!this.resetAnim);
+  }
+
+  moveTo(slide) {
+    this.myCarousel.moveTo(slide, !this.withAnim);
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,14 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TabComponent } from "./tab/tab.component";
 import { TabsComponent } from "./tabs/tabs.component";
+import { NguCarouselModule } from "@ngu/carousel";
 
-const MODULES = [CommonModule, FormsModule, ReactiveFormsModule];
+const MODULES = [
+  CommonModule,
+  FormsModule,
+  ReactiveFormsModule,
+  NguCarouselModule,
+];
 const DIRECTIVES = [];
 const COMPONENTS = [TabsComponent, TabComponent];
 
