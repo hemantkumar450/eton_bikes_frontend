204c0f21de0e98e1883a83be33e1e090c6257311
hemantt9l <hemant@t9l.com>
2020-10-03 17:55:06 +0530

    changes

--- src/app/modules/bike/detail/detail-images/detail-images.component.ts
+++ src/app/modules/bike/detail/detail-images/detail-images.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-detail-images',
-  templateUrl: './detail-images.component.html',
-  styleUrls: ['./detail-images.component.scss']
+  selector: "app-detail-images",
+  templateUrl: "./detail-images.component.html",
+  styleUrls: ["./detail-images.component.scss"],
 })
 export class DetailImagesComponent implements OnInit {
+  images = [
+    "/assets/img/files/toptube_bs.jpg",
+    "/assets/img/files/chainstay_detail.jpg",
+    "/assets/img/files/flipchip_29.jpg",
+    "/assets/img/files/tire_clearance_grey.jpg",
+    "/assets/img/files/dhx2_coil_bs.jpg",
+    "/assets/img/files/downtube_protector.jpg",
+    "/assets/img/files/matte_headtube.jpg",
+    "/assets/img/detail/chain_slapper.jpg",
+    "/assets/img/files/internal_routing.jpg",
+    "/assets/img/files/vpp_1.jpg",
+    "/assets/img/files/my21_v10mx_details_001.jpg",
+    "/assets/img/files/my21_v10mx_details_004.jpg",
+    "/assets/img/files/my21_v10mx_details_005.jpg",
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/bike/detail/overview/overview.component.ts
+++ src/app/modules/bike/detail/overview/overview.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-overview',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+  selector: "app-overview",
+  templateUrl: "./overview.component.html",
+  styleUrls: ["./overview.component.scss"],
 })
 export class OverviewComponent implements OnInit {
+  images = [
+    "/assets/img/files/ftwilliam19_h1d6041_1.jpg",
+    "/assets/img/files/ftwilliam19a63i0632_1.jpg",
+    "/assets/img/files/ftwilliam19bb019807_0.jpg",
+    "/assets/img/files/ftwilliam19_h1d7631_0.jpg",
+    "/assets/img/files/ftwilliam19_m1_0797_2.jpg",
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
