cad3a25d7ac1a6727c6e7b5b13af30e8e38cccdd
hemantkumar450 <38126340+hemantkumar450@users.noreply.github.com>
2020-11-25 22:08:25 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndPoint: "http://148.72.208.32:5000/api/",
-  filePath: "http://148.72.208.32:5000/",
+  apiEndPoint: "https://148.72.208.32:5000/api/",
+  filePath: "https://148.72.208.32:5000/",
   noImagePath: "/assets/images/default/no-image.png"
 };
